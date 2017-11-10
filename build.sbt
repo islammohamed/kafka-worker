e86f1a1055d269080fccc0468ed385fbdf51c98f
Islam Mohamed <iabdelaziz@me.com>
2017-11-10 14:33:38 +0100

    add the first non working v

--- build.sbt
+++ build.sbt
@@ -1,12 +1,17 @@
 import Dependencies._
+import sbt.Keys.libraryDependencies
 
 lazy val root = (project in file(".")).
   settings(
     inThisBuild(List(
-      organization := "com.example",
+      organization := "com.ocono",
       scalaVersion := "2.12.3",
-      version      := "0.1.0-SNAPSHOT"
+      version := "0.1.0-SNAPSHOT"
     )),
     name := "Hello",
-    libraryDependencies += scalaTest % Test
-  )
+    libraryDependencies ++= Seq(
+      "org.apache.kafka" % "kafka_2.11" % "0.8.2.1",
+      "org.apache.kafka" % "kafka-streams" % "1.0.0",
+      "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+)
+)
--- src/main/scala/com/ocono/KafkaSteamWorker.scala
+++ src/main/scala/com/ocono/KafkaSteamWorker.scala
@@ -1,10 +1,40 @@
 package com.ocono
 
-class KafkaStreamWorker {
+import java.util.Properties
 
+import org.apache.kafka.common.serialization._
+import org.apache.kafka.common.utils.Bytes
+import org.apache.kafka.streams._
+import org.apache.kafka.streams.kstream._
+import org.apache.kafka.streams.kstream.internals.PrintForeachAction
+import org.apache.kafka.streams.state.KeyValueStore
+
+import scala.collection.JavaConverters.asJavaIterableConverter
+
+
+class KafkaStreamWorker {
+  import KafkaStreamWorker._
+
+  val config: Properties = {
+    val p = new Properties()
+    p.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationName)
+    p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    p.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+    p.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+    p
+  }
+
+  val builder: StreamsBuilder = new StreamsBuilder()
+  val textLines: KStream[String, String] = builder.stream(topic)
+  
+
+  val streams: KafkaStreams = new KafkaStreams(builder.build(), config)
+  streams.start()
 }
 
 object KafkaStreamWorker {
+  val topic:String = "teamk"
+  val applicationName:String = "kafka-worker"
 
   def apply(kafkaClusterConfig: KafkaClusterConfig): KafkaStreamWorker = {
     new KafkaStreamWorker()
